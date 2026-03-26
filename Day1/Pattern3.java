f62584b1204f8c85538f6d604deeeb03e85d06af
Sanjay Kumar K <sanjaykrishnakumar01@gmail.com>
2026-03-26 12:33:08 +0530

    DAY1

---
+++ Day1/Pattern3.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Pattern3{
+    public static void main(String[] args) {
+        int count=1;
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int space=n-1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=space;j++){
+                System.out.print(" ");
+            }
+            space--;
+            for(int k=1;k<=count;k++){
+                System.out.print("*");
+            }
+            count+=2;
+            System.out.println();
+        }
+    }
+}
---
+++ Day1/implicit.java
@@ -0,0 +1,7 @@
+public class implicit{
+    public static void main(String args[]){
+        double num1=10.0;
+        int num2=(int)num1;
+        System.err.println(num2);
+    }
+}
